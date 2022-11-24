2b1454b0dfae6e3a7c34090a5ce5f883e69f5b30
David Cheng <32114983+ChengDave@users.noreply.github.com>
2022-11-23 21:41:39 -0700

    initial setup of file structure, java spring, react-create-app and gitignore

---
+++ backend/Movie-Theater-Ticket-App/src/main/java/com/example/MovieTheaterTicketApp/MovieTheaterTicketAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.MovieTheaterTicketApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieTheaterTicketAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieTheaterTicketAppApplication.class, args);
+	}
+
+}
---
+++ backend/Movie-Theater-Ticket-App/src/test/java/com/example/MovieTheaterTicketApp/MovieTheaterTicketAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MovieTheaterTicketApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieTheaterTicketAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
