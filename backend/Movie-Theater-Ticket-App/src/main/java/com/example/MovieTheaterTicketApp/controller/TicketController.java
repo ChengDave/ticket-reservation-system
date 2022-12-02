47a34f86a57482e49c9183d5e4974f411b2993ab
David Cheng <32114983+ChengDave@users.noreply.github.com>
2022-12-02 14:59:19 -0700

    added seat taken logic

--- backend/Movie-Theater-Ticket-App/src/main/java/com/example/MovieTheaterTicketApp/controller/TicketController.java
+++ backend/Movie-Theater-Ticket-App/src/main/java/com/example/MovieTheaterTicketApp/controller/TicketController.java
@@ -3,6 +3,8 @@ package com.example.MovieTheaterTicketApp.controller;
 import java.util.List;
 import java.util.Optional;
 
+import com.example.MovieTheaterTicketApp.model.Seat;
+import com.example.MovieTheaterTicketApp.service.SeatService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,6 +15,8 @@ import com.example.MovieTheaterTicketApp.service.TicketService;
 @RestController
 public class TicketController {
     TicketService ticketService;
+    SeatService seatService;
+
 
     @Autowired
     public TicketController(TicketService ticketService) {
@@ -21,11 +25,19 @@ public class TicketController {
     
     @PostMapping
     public void addTicket(@RequestBody Ticket ticket){
+        Seat seat = ticket.getSeat();
+        seatService.registerSeat(seat); // sets seat to istaken
         ticketService.addTicket(ticket);
     }
 
-    @DeleteMapping
-    public void deleteTicketById(@RequestBody Long id){
+//    @DeleteMapping
+//    public void deleteTicket(@RequestBody Ticket ticket){
+//        Seat seat = ticket.getSeat();
+//        seatService.registerSeat(seat); // sets seat to istaken
+//    }
+
+    @DeleteMapping(path = "/{id}")
+    public void deleteTicketById(@PathVariable("id") Long id){
         ticketService.deleteTicketById(id);
     }
 
