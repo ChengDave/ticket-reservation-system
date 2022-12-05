9a6f5fcee69f87a8b8be4fc70f0d1a7c0a02d575
David Cheng <32114983+ChengDave@users.noreply.github.com>
2022-12-04 20:18:59 -0700

    added comments to frontend main files

--- frontend/ticket-frontend/src/APICalls/GetUser.js
+++ frontend/ticket-frontend/src/APICalls/GetUser.js
@@ -1,6 +1,6 @@
 
 export const getUserByEmail = async (email) => {
-	
+	// get user by email
 	let response = await fetch("http://localhost:8080/api/v1/users/email/"+ email, {})
 	let user = await response.json()
 
--- frontend/ticket-frontend/src/APICalls/getTicketsByUserID.js
+++ frontend/ticket-frontend/src/APICalls/getTicketsByUserID.js
@@ -1,6 +1,6 @@
 
 export const getTicketsByUserID = async (userId) => {
-	
+	// get tickets by user
 	let response = await fetch("http://localhost:8080/api/v1/ticket/user/"+ userId, {})
 	let tickets = await response.json()
 
